2e90ded37fbd0091f43cdd24795e3983960030d7
SimonAllenOliver <simonallenoliver@gmail.com>
2024-01-09 15:09:41 -0600

    updated

--- assignment_user.py
+++ assignment_user.py
@@ -14,6 +14,7 @@ class User:
         print(self.age)
         print(self.is_rewards_member)
         print(self.gold_card_points)
+        return self
 # Have this method change the user's member status to True and set their gold card points to 200. 
 # Add logic in the enroll method to check if they are a member already, and if they are, print "User already a member."
     def enroll(self):
@@ -22,6 +23,7 @@ class User:
             self.gold_card_points = 200
         else:
             print("User already a member")
+        return self
 # Have this method decrease the user's points by the amount specified. 
 # Add logic in the spend points method to be sure they have enough points to spend that amount and handle appropriately.
     def spend_points(self, amount):
@@ -29,14 +31,8 @@ class User:
             self.gold_card_points -= amount
         else:
             print("You don't have enough points.")
+        return self
 
 user_simon = User("Simon", "Oliver", "simon@email.com", 26)
 
-User.display_info(user_simon)
-User.enroll(user_simon)
-User.display_info(user_simon)
-User.spend_points(user_simon, 50)
-User.display_info(user_simon)
-User.spend_points(user_simon, 250)
-User.display_info(user_simon)
-User.enroll(user_simon)
+user_simon.display_info().enroll().spend_points(50).display_info().enroll().spend_points(250)
